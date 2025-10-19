d24a3c02a6a8e3b0d025a1d09e08b6fa3b8c304c
axelPortatil <axelperez221105@gmail.com>
2025-10-19 10:57:30 +0300

    solving problem when techs are not seen in project details when screen has lower height

--- src/presentation/components/FlatlistTech.tsx
+++ src/presentation/components/FlatlistTech.tsx
@@ -9,7 +9,7 @@ export const FlatListTech = ({techStack}: Props) => {
     return (
         <div className="flex flex-wrap gap-3 h-auto">
             {techStack.map((tech, index) => (
-                <div data-aos={"fade-left"}
+                <div
                      key={index}
                      className="tech-card-container h-10 text-white px-4 py-2 rounded-xl shadow-md"
                 >
