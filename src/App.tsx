3d8491c409a922b1a1083fe48438a0050d907abf
axelPortatil <axelperez221105@gmail.com>
2025-10-19 11:42:13 +0300

    fixing error with structure of App.tsx and main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
 import './App.css'
 import Home from "./presentation/sections/home/Home.tsx";
 import AboutMe from "./presentation/sections/about-me/AboutMe.tsx";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import 'aos/dist/aos.css';
 import Projects from "./presentation/sections/projects/Projects.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProjectDetails from "./presentation/views/project-details/ProjectDetails.tsx";
 import SideTabBar from "./presentation/components/side-tab-bar/SideTabBar.tsx";
 import Contact from "./presentation/sections/contact/Contact.tsx";
-
 
 function App() {
     useEffect(() => {
         AOS.init({ duration: 800, once: false, mirror: true });
         window.addEventListener('resize', () => AOS.refresh());
-
         return () => window.removeEventListener('resize', () => AOS.refresh());
     }, []);
-  return (
-    <BrowserRouter>
+
+    return (
         <Routes>
-            <Route path="/"
-                    element={
+            <Route
+                path="/"
+                element={
                     <>
-                        <SideTabBar/>
-                        <Home/>
-                        <AboutMe/>
-                        <Projects/>
-                        <Contact/>
+                        <SideTabBar />
+                        <Home />
+                        <AboutMe />
+                        <Projects />
+                        <Contact />
                     </>
-                   }/>
-            <Route path={"/projects/:slug"} element={<ProjectDetails/>}/>
+                }
+            />
+            <Route path="/projects/:slug" element={<ProjectDetails />} />
         </Routes>
-    </BrowserRouter>
-  )
+    )
 }
 
 export default App
