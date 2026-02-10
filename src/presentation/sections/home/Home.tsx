8e5e6dedb7ae12a9eb45bf8dd536c6f0cd45ed5b
axelPortatil <axelperez221105@gmail.com>
2026-02-10 13:08:34 +0100

    updating cv file 3.

--- src/presentation/sections/home/Home.tsx
+++ src/presentation/sections/home/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
 
     const handleCVDownload = () => {
         const link = document.createElement("a");
-        link.href = "../../../public/cv-axel-rojas-en-s.pdf";
+        link.href = "/cv-axel-rojas-en-s.pdf";
         link.download = "cv-axel-rojas-en-s.pdf";
         link.click();
     }
